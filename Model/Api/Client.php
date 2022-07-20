dbcdc9dd73c4dd411da510773bb1b75f004d8e68
Andrii <chaikovskyia@ukr.net>
2022-07-20 11:19:44 +0300

    Days variable fixed, version updated

--- Model/Api/Client.php
+++ Model/Api/Client.php
@@ -253,7 +253,7 @@ class Client
             'Dintero-System-Name' => __('Magento'),
             'Dintero-System-Version' => $this->getSystemMeta()->getVersion(),
             'Dintero-System-Plugin-Name' => 'Dintero.Checkout.Magento.V2',
-            'Dintero-System-Plugin-Version' => '1.6.14',
+            'Dintero-System-Plugin-Version' => '1.6.15',
         ];
 
         if ($token && $token instanceof Token) {
